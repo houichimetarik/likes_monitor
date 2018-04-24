0b00867ce1c8ed984adf5a89b9b3fa7eb1523d26
houichimetarik <38704422+houichimetarik@users.noreply.github.com>
2018-04-24 21:53:06 +0100

    Update TwitterProvider.class.php

--- providers/TwitterProvider.class.php
+++ providers/TwitterProvider.class.php
@@ -5,6 +5,15 @@ include "Provider.class.php";
 // Twitter data provider using Twitter API
 class TwitterProvider extends Provider{
 
+  function fetch_twitter_count($user) {
+
+    $json = file_get_contents("http://api.twitter.com/1/users/show.json?screen_name=".$user."");
+
+    $count = json_decode($json, true); // json strint
+
+    return intval($count['followers_count']); // here is your integer
+}
+  
 } // end class TwitterProvider
 
 ?>
